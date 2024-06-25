031ae20dce8460d4609f912b2e70e529294bd521
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-25 21:51:16 +0600

    made slider responsive

--- src/components/Home/Slider/Slide1.jsx
+++ src/components/Home/Slider/Slide1.jsx
@@ -5,14 +5,16 @@ const Slide1 = () => {
   return (
     <div
       style={{ backgroundImage: `url(${slide1})` }}
-      className="flex flex-col justify-center h-[200px] lg:h-[400px] bg-cover bg-center bg-no-repeat rounded-lg"
+      className="flex flex-col justify-center h-[250px] lg:h-[400px] bg-cover bg-center bg-no-repeat rounded-lg"
     >
       <div className="p-[30px] lg:p-[100px]">
         <h3 className="text-[22px] font-medium text-accent">New Camera. New Design</h3>
-        <h1 className="text-[40px] font-bold text-white">iPhone 15 Pro Max</h1>
-        <hr className="w-16 border-2 border-gray-500 my-4" />
-        <p className="text-[20px] text-white mb-4">Titanium So Strong. So Light. So Pro.</p>
-        <Button text={"Shop Now"} link={"/"} />
+        <h1 className="text-[36px] lg:text-[40px] font-bold text-white">iPhone 15 Pro Max</h1>
+        <hr className="w-16 border-2 border-gray-500 my-4 hidden lg:block" />
+        <p className="text-[20px] text-white hidden lg:block">Titanium So Strong. So Light. So Pro.</p>
+        <div className="mt-4">
+          <Button text={"Shop Now"} link={"/"} />
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,7 @@ const Home = () => {
         <NewArrivalProducts />
         <Gap />
         <Slider />
+        <Gap />
       </div>
     </div>
   );
