3beb01b893331e490bf988bd6d18ae96793785bd
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-06-26 17:51:24 +0600

    added image title and category to blog card

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,11 +1,12 @@
-import banner4 from "../assets/Banner/Banner-4.jpeg";
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
 
-const BlogCard = () => {
+const BlogCard = ({ image, title, category }) => {
   return (
-    <div>
-      <div className="relative">
-        <img src={banner4} className="h-[215px] w-full rounded-lg" />
-        <p className="absolute bottom-3 left-3 text-xs bg-primary px-2 py-1 rounded text-white">Audio Electronics</p>
+    <Link to="/">
+      <div className="relative rounded-lg">
+        <img src={image} className="h-[215px] w-full rounded-lg" />
+        <p className="absolute bottom-3 left-3 text-xs bg-primary px-2 py-1 rounded text-white">{category}</p>
         <div className="absolute top-3 left-3 bg-white w-14 h-14 flex items-center justify-center rounded-full">
           <p className="text-center leading-[14px] mt-2">
             <span className="font-semibold text-[22px]">02</span> <br />
@@ -13,11 +14,11 @@ const BlogCard = () => {
           </p>
         </div>
       </div>
-      <h3 className="text-[17px] font-medium mt-4">Announcing the new Fitbits Charge</h3>
+      <h3 className="text-[17px] font-medium mt-4">{title}</h3>
       <p className="text-[14px] text-gray-500 mt-1">
         Recentley I was invited by Nintendo of Canada to attend a very special Nintendo Holiday Showcase
       </p>
-    </div>
+    </Link>
   );
 };
 
--- src/components/Home/From Our Articles/FromOurArticles.jsx
+++ src/components/Home/From Our Articles/FromOurArticles.jsx
@@ -1,15 +1,19 @@
 import BlogCard from "../../BlogCard";
 import SectionTitle from "../../Common/SectionTitle";
+import blog1 from "../../../assets/Blogs/blog1.jpg";
+import blog2 from "../../../assets/Blogs/blog2.jpg";
+import blog3 from "../../../assets/Blogs/blog3.jpg";
+import blog4 from "../../../assets/Blogs/blog4.jpg";
 
 const FromOurArticles = () => {
   return (
     <div>
       <SectionTitle title={"From Our Articles"} />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-6">
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 lg:gap-5">
+        <BlogCard image={blog1} category={"Audio Electronics"} />
+        <BlogCard image={blog2} category={"Audio Electronics"} />
+        <BlogCard image={blog3} category={"Audio Electronics"} />
+        <BlogCard image={blog4} category={"Audio Electronics"} />
       </div>
     </div>
   );
